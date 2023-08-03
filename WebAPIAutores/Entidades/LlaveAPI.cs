2bf8787e15812ec20dca66091cb0a9e55f3d7e96
ribeiromauri <110779735+ribeiromauri@users.noreply.github.com>
2023-08-03 14:54:15 -0300

    Agregar archivos de proyecto.

---
+++ WebAPIAutores/Entidades/LlaveAPI.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebAPIAutores.Entidades
+{
+    public class LlaveAPI
+    {
+        public int Id { get; set; }
+        public string Llave { get; set; }
+        public TipoLlave TipoLlave { get; set; }
+        public bool Activa { get; set; }
+        public string UsuarioId { get; set; }
+        public IdentityUser Usuario { get; set; }
+    }
+
+    public enum TipoLlave
+    {
+        Gratis = 1,
+        Profesional = 2
+    }
+}
